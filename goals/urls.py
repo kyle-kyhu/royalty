4b41b1b8ec85c7319db88754681383b77148fd7d
kyle-kyhu <kylehunt22@gmail.com>
2024-01-14 21:21:54 -0500

    urls and pk functionally is not working

--- goals/urls.py
+++ goals/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from goals.views import GoalsPageView, GoalsDetailView
+from goals.views import GoalsPageView, GoalsUpdateView
 
 app_name = "goals"
 urlpatterns = [
     path("", GoalsPageView.as_view(), name="goals"),
-    path("goals/<int:pk>/", GoalsDetailView.as_view(), name="goals_detail"),
+    path("goals/<int:pk>/", GoalsUpdateView.as_view(), name="goals_update"),
 ]
--- goals/views.py
+++ goals/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic.edit import FormView, UpdateView
+
 
 from .models import Goals
 from .forms import GoalsForm
 
-# your stuff
 
 class GoalsPageView(FormView):
     template_name = "goals/goals.html"
@@ -13,14 +12,19 @@ class GoalsPageView(FormView):
     form_class = GoalsForm
 
     def form_valid(self, form):
+        form.instance.user = self.request.user
         form.save()
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('home')
 
 
-class GoalsDetailView(UpdateView):
+class GoalsUpdateView(UpdateView):
     model = Goals
-    template_name = "goals/goals_detail.html"
+    template_name = "goals/goals_update.html"
     form_class = GoalsForm
+    context_object_name = 'object'
 
     def get_success_url(self):
-        return reverse('users:goals_detail', kwargs={'pk': self.object.pk})
+        return reverse('home')
